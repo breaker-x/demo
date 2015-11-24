826a7c26625b184d7f36e7d9a5a5deecf35c31a1
xul <xul@xul-PC>
2015-11-24 11:42:43 +0800

    加入往txt文档里写内容的工具类

---
+++ src/main/java/com/bx/common/utils/IOUtil.java
@@ -0,0 +1,74 @@
+package com.bx.common.utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class IOUtil {
+	 /**
+     * 写文件
+     * 
+     * @param newStr
+     *            新内容
+     * @throws IOException
+     */
+    public static void writeTxtFile(String newStr, String filenameTemp) throws IOException {
+        // 先读取原有文件内容，然后进行写入操作
+        String filein = newStr + "\r\n";
+        String temp = "";
+
+        FileInputStream fis = null;
+        InputStreamReader isr = null;
+        BufferedReader br = null;
+
+        FileOutputStream fos = null;
+        PrintWriter pw = null;
+        try {
+            // 文件路径
+            File file = new File(filenameTemp);
+            // 将文件读入输入流
+            fis = new FileInputStream(file);
+            isr = new InputStreamReader(fis);
+            br = new BufferedReader(isr);
+            StringBuffer buf = new StringBuffer();
+
+            // 保存该文件原有的内容
+            for (int j = 1; (temp = br.readLine()) != null; j++) {
+                buf = buf.append(temp);
+                // System.getProperty("line.separator")
+                // 行与行之间的分隔符 相当于“\n”
+                buf = buf.append(System.getProperty("line.separator"));
+            }
+            buf.append(filein);
+
+            fos = new FileOutputStream(file);
+            pw = new PrintWriter(fos);
+            pw.write(buf.toString().toCharArray());
+            pw.flush();
+        } catch (IOException e1) {
+            // TODO 自动生成 catch 块
+            throw e1;
+        } finally {
+            if (pw != null) {
+                pw.close();
+            }
+            if (fos != null) {
+                fos.close();
+            }
+            if (br != null) {
+                br.close();
+            }
+            if (isr != null) {
+                isr.close();
+            }
+            if (fis != null) {
+                fis.close();
+            }
+        }
+    }
+
+}
